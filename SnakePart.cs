e09e13abc12341f1f02b8b1a5a31643c4246c7ea
SASHALIK1 <alexsun2006.ukr@gmail.com>
2025-04-15 22:42:29 +0300

    chore: SnakePart class created for future game logic

---
+++ SnakePart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_game
+{
+    internal class SnakePart
+    {
+        public int X;
+        public int Y;
+        public char Shape;
+        public SnakePart(int x, int y, char shape)
+        {
+            X = x;
+            Y = y;
+            Shape = shape;
+        }
+        public SnakePart(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
