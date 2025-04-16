48460d8a3c1d4338a0384daeee7f6e51f76cda7b
SASHALIK1 <alexsun2006.ukr@gmail.com>
2025-04-16 13:39:24 +0300

    refactor: change field access modifiers in Fruit class

--- Fruit.cs
+++ Fruit.cs
@@ -9,7 +9,7 @@ namespace Snake_game
     internal class Fruit : GameObject
     {
         private static readonly Random _random = new Random();
-        private readonly char[] _shapes = ['*', 'o', 'a'];
+        private static readonly char[] _shapes = ['*', 'o', 'a'];
         public Fruit(int x, int y)
         {
             X = x;
