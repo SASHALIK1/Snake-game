925a85fdee8e5590dd24cefb5dc64ad7aa47f441
SASHALIK1 <alexsun2006.ukr@gmail.com>
2025-04-16 13:58:16 +0300

    refactor: Improved readability of FruitSpawner

--- FruitSpawner.cs
+++ FruitSpawner.cs
@@ -19,9 +19,8 @@ namespace Snake_game
             {
                 randomX = _random.Next(1, Console.WindowWidth - 1);
                 randomY = _random.Next(1, Console.WindowHeight - 1);
-                newFruitPosition = new SnakePart(randomX, randomY);
             }
-            while (snakeParts.Contains(newFruitPosition));
+            while (snakeParts.Contains(new SnakePart(randomX, randomY)));
 
             return new Fruit(randomX, randomY);
         }
